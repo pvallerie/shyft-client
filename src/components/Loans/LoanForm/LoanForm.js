1ff7af2d47f38efde85ba2e5c670131b034f6adf
pvallerie <petevallerie@gmail.com>
2021-02-19 15:15:02 -0500

    Add CreateLoan functionality to ShowPost component
    - refactor CreateLoan into LoanForm
    - refactor CreateLoan functionality in to ShowBike component

---
+++ src/components/Loans/LoanForm/LoanForm.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const LoanForm = props => {
+  const { handleChange, handleSubmit } = props
+  // const [createdId, setCreatedId] = useState(null)
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicPickUpDate">
+          <Form.Label>Pickup Date</Form.Label>
+          <Form.Control
+            type="date"
+            name="pickup_date"
+            placeholder="YYYY-MM-DD"
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicDropOffDate">
+          <Form.Label>Dropoff Date</Form.Label>
+          <Form.Control
+            type="date"
+            name="dropoff_date"
+            placeholder="YYYY-MM-DD"
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          type="submit"
+        >
+          Rent it!
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
+export default LoanForm
